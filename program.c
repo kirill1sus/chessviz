f6eac5c8ae35283a8e2ff58014ee2207fc886346
Kirill Sus <k.sus12@mail.ru>
2018-02-26 08:46:40 +0700

    The file displays the inital state of the board

---
+++ program.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main {
+    printf("r n b q k b n r");
+    printf("p p p p p p p p");
+    printf("\n\n\n\n");
+    printf("R N B Q K B N R");
+    printf("P P P P P P P P");
+}
